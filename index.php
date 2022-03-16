2ca65f3c8c224a8f1755bb2d553a462d73cc2435
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-03-16 11:29:43 +0100

    Aanpasing index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once 'backend/config.php';
 
 
-echo $base_url;
+
 
 ?>
 
@@ -16,7 +16,7 @@ echo $base_url;
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body class="mainpage">
-<?php require_once '../layouts/header.php'; ?>
+<?php require_once 'layouts/header.php'; ?>
 <div class="wrapper">
     <div class="datum_tijd">
         <iframe src="https://free.timeanddate.com/clock/i80s3idm/n1297/tlaw14/fn6/fs16/fcfff/tc000/ftb/bas2/bat1/bacfff/pa8/tt0/tw1/th1/ta1/tb4" frameborder="0" width="207" height="58"></iframe>
@@ -43,6 +43,6 @@ echo $base_url;
   </div>
 </div>
 </div>
-<?php require_once '../layouts/footer.php'; ?>
+<?php require_once 'layouts/footer.php'; ?>
 </body>
 
