156515b6ab8abc9f78f86fb5484aa662b2c864c0
Wouterpiso <wpiso2000@gmail.com>
2022-03-16 12:41:00 +0100

    start pagina songteksten toegevoegd

---
+++ songteksten/songteksten.php
@@ -0,0 +1,16 @@
+<?php require_once '../backend/config.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="<?php echo $base_url ?>../css/stylesheet.css">
+</head>
+<body class="mainpage">
+<?php require_once '../layouts/header.php'; ?>
+
+<p>test</p>
+
+<?php require_once '../layouts/footer.php'; ?>
+</body>
+
+</html>
