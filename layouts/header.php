e3e7a653b013f0369ded99e89c6000264656880a
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-03-16 11:58:45 +0100

    Header basis
    De basis van de header

--- layouts/header.php
+++ layouts/header.php
@@ -1,1 +1,24 @@
+<<<<<<< Updated upstream
 <h1>CodeCamp</h1>
+=======
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href=css/style.css>
+</head> 
+<header>
+    <div class="logo">   <a href="/"><img src="../img/code-camp.png" alt="logo" width='150px' height="150px"></a></div>
+<div class="links">
+
+
+    <div class="demo">
+    <button><a href="../dranken/dranken.php">Dranken</a></button>
+            <button><A href="../songteksten/songteksten.php"> Songteksten</a> </button>
+            <button><a href="../gereedschap/Gereedschap.php">Gereedschap</a></button>
+            <button><a href="../acteurs/acteurs.php">Acteurs</a></button>
+            <button><a href="../boeken/boeken.php">Boeken</a></button>
+            
+    </div>
+</div>
+</header>
+>>>>>>> Stashed changes
