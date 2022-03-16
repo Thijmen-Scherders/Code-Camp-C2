68fc4ae012784fbbc27694e2c04ef34021ddac9a
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-16 12:16:08 +0100

    Slight cleanup

--- index.php
+++ index.php
@@ -1,48 +1,41 @@
-<?php
-
-require_once 'backend/config.php';
-
-
-
-
-?>
+<?php require_once 'backend/config.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
-
-</html>
 <head>
-    <link rel="stylesheet" href="<?php echo $base_url ?>/css/style.css">
+	<link rel="stylesheet" href="<?php echo $base_url ?>/css/style.css">
 </head>
 <body class="mainpage">
 <?php require_once 'layouts/header.php'; ?>
 <div class="wrapper">
-    <div class="datum_tijd">
-        <iframe src="https://free.timeanddate.com/clock/i80s3idm/n1297/tlaw14/fn6/fs16/fcfff/tc000/ftb/bas2/bat1/bacfff/pa8/tt0/tw1/th1/ta1/tb4" frameborder="0" width="207" height="58"></iframe>
-
-    
-    </div>
-    <div class="homeimg">
+	<div class="datum_tijd">
+		<iframe src="https://free.timeanddate.com/clock/i80s3idm/n1297/tlaw14/fn6/fs16/fcfff/tc000/ftb/bas2/bat1/bacfff/pa8/tt0/tw1/th1/ta1/tb4"
+				frameborder="0" width="207" height="58"></iframe>
 
 
-    <div class="contact">
-            <h1>Where can you find us.</h1><br>
-            <h2>Adres:Koestraat 1<br>
-                den Bosch<br>
-                Phone:0882097521<br>
-                Email Addrres:tscherders@kpnmail.nl<br>
-            </h2>
-            <a target="_blank" href="https://www.google.com/maps/place/Koestraat+1,+5223+AG+'s-Hertogenbosch/@51.697836,5.2876068,15.42z/data=!4m5!3m4!1s0x47c6ee66bc48c005:0x4dddc528356e3d4f!8m2!3d51.6973624!4d5.2935999">
-            <img src="img/map.png" alt="map" a>
-            </a>
+	</div>
+	<div class="homeimg">
 
 
-    </div>
+		<div class="contact">
+			<h1>Where can you find us.</h1><br>
+			<h2>Adres:Koestraat 1<br>
+				den Bosch<br>
+				Phone:0882097521<br>
+				Email Addrres:tscherders@kpnmail.nl<br>
+			</h2>
+			<a target="_blank"
+			   href="https://www.google.com/maps/place/Koestraat+1,+5223+AG+'s-Hertogenbosch/@51.697836,5.2876068,15.42z/data=!4m5!3m4!1s0x47c6ee66bc48c005:0x4dddc528356e3d4f!8m2!3d51.6973624!4d5.2935999">
+				<img src="img/map.png" alt="map" a>
+			</a>
 
-  </div>
+
+		</div>
+
+	</div>
 </div>
 </div>
 <?php require_once 'layouts/footer.php'; ?>
 </body>
 
+</html>
