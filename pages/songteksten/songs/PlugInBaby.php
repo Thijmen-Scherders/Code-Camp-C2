f246e22156b96b3df059d9f093c81e0b5826c121
Wouterpiso <wpiso2000@gmail.com>
2022-03-19 12:17:16 +0100

    eerste song text pagina + css gemaakt (temp)

---
+++ pages/songteksten/songs/PlugInBaby.php
@@ -0,0 +1,65 @@
+<?php
+	$path = $_SERVER['DOCUMENT_ROOT'];
+	$path .= "/backend/config.php";
+	include_once($path);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="<?php echo baseUrl() ?>public/stylesheet.css">
+</head>
+<body class="mainpage">
+
+<?php includeHeader(); ?>
+
+<div class="wrapper">
+	<div class="container-column orange">
+		<h1>Muse - Plug in baby</h1>
+		<p> 
+			I've exposed your lies
+			Baby
+			The underneath's no big surprise
+			Now it's time for changing
+			And cleansing everything to forget your love
+		</p> 
+		<p> 
+			My plug in baby
+			Crucifies my enemies
+			When I'm tired of giving
+			My plug in baby
+			In unbroken virgin realities
+			Is tired of living
+		</p> 
+		<p> 
+			Don't confuse
+			Baby your gonna lose
+			Your own game
+			Change me
+			Replace the envying
+			To forget your love
+		</p> 
+		<p> 
+			My plug in baby
+			Crucifies my enemies
+			When I'm tired of giving
+			My plug in baby
+			In unbroken virgin realities
+			Is tired of living
+		</p> 
+		<p> 
+			And I've seen your loving
+			Mine is gone
+		</p> 
+		<p> 
+			And I've been in trouble
+			wooaahhhh
+		</p>
+	</div>
+</div>
+
+
+<?php includeFooter(); ?>
+</body>
+
+</html>
