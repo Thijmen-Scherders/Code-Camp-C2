1b5e02e0c8e147956f916b5dbd1e4ffd0f19ed96
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-03-21 12:32:04 +0100

    kettingzaag pagina aangepast
    nog niet helemaal klaar maar beter

--- pages/gereedschap/zagen/kettingzagen/index.php
+++ pages/gereedschap/zagen/kettingzagen/index.php
@@ -1,23 +1,48 @@
-<h1>kettingzagen</h1>
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/backend/config.php";
+include_once($path);
+?>
 
-<h2>Een kettingzaag (ook motorzaag, motorkettingzaag of boomzaag genoemd) 
-    is een draagbare machine om mee te zagen. De belangrijkste functie is 
-    hout zagen.</h2>
-<h2>Elektrische kettingzaag</h2>
-<h2>Elektrische kettingzagen zijn met name bedoeld voor particulier gebruik. 
-    Ze zijn milieuvriendelijker en maken doorgaans minder lawaai dan 
-    kettingzagen op benzine. Ze zijn praktischer in gebruik doordat ze 
-    lichter zijn. Elektrische kettingzagen zijn vaak goedkoper in aanschaf. 
-    Nadeel is het snoer bij sommige elektrische kettingzagen waardoor de 
-    actieradius beperkt is, maar draadloze elektrische kettingzagen bestaan ook.
-    Deze werken op een accu.</h2>
-<h2>Kettingzaag met brandstofmotor</h2>
-<h2>Benzinekettingzagen hebben bijna zonder uitzondering een tweetaktmotor. 
-    Benzine-aangedreven kettingzagen hebben het voordeel dat ze een grotere 
-    mobiliteit hebben en krachtiger zijn dan elektrische kettingzagen.
-    Er zijn verschillende typen benzine-kettingzaag verkrijgbaar:
-    Gewone motorkettingzaag = variërend van 30 cc tot 121 cc.
-    Tophandle = licht type kettingzaag. Hoewel deze machines bediend 
-    kunnen worden met één hand, is dit verboden. Wordt vooral gebruikt in 
-    de fruitteelt en de boomverzorging. motorstokzaag = kettingzaag op een 
-    stok om takken op hoogte door te zagen.</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="<?php echo baseUrl() ?>/public/stylesheet.css">
+	<title>Songteksten</title>
+</head>
+
+<body class="mainpage">
+
+<?php includeHeader(); ?>   
+    <div class="kettingzaag">
+        <h1>kettingzagen</h1>
+        <img src="/public/kettingzaag.jpg" alt="kettingzaag" a>
+        <h2>Een kettingzaag (ook motorzaag, motorkettingzaag of boomzaag genoemd) 
+        is een draagbare machine om mee te zagen. De belangrijkste functie is 
+        hout zagen.</h2>
+        <h2>Elektrische kettingzaag</h2>
+        <img src="/public/elektrisch.jpg" alt="elektrische zaag" a>
+    <   h2>Elektrische kettingzagen zijn met name bedoeld voor particulier gebruik. 
+        Ze zijn milieuvriendelijker en maken doorgaans minder lawaai dan 
+        kettingzagen op benzine. Ze zijn praktischer in gebruik doordat ze 
+        lichter zijn. Elektrische kettingzagen zijn vaak goedkoper in aanschaf. 
+        Nadeel is het snoer bij sommige elektrische kettingzagen waardoor de 
+        actieradius beperkt is, maar draadloze elektrische kettingzagen bestaan ook.
+        Deze werken op een accu.</h2>
+        <h2>Kettingzaag met brandstofmotor</h2>
+        <img src="/public/brandstof.jpg" alt="brandstofmotorzaag" a>
+        <h2>Benzinekettingzagen hebben bijna zonder uitzondering een tweetaktmotor. 
+        Benzine-aangedreven kettingzagen hebben het voordeel dat ze een grotere 
+        mobiliteit hebben en krachtiger zijn dan elektrische kettingzagen.
+        Er zijn verschillende typen benzine-kettingzaag verkrijgbaar:
+        Gewone motorkettingzaag = variërend van 30 cc tot 121 cc.
+        Tophandle = licht type kettingzaag. Hoewel deze machines bediend 
+        kunnen worden met één hand, is dit verboden. Wordt vooral gebruikt in 
+        de fruitteelt en de boomverzorging. motorstokzaag = kettingzaag op een 
+        stok om takken op hoogte door te zagen.</h2>
+    </div>
+    <?php includeFooter(); ?>
+
+</body>
+
+</html>
