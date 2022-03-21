c7c13ef2d4afb1484599d9edec3e40a7e25c23be
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-21 12:46:35 +0100

    Menu header redesign

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ include_once($path);
 <?php includeHeader(); ?>
 
 <div class="wrapper">
-	<div class="datum_tijd">
-		<iframe src="https://free.timeanddate.com/clock/i80s3idm/n1297/tlaw14/fn6/fs16/fcfff/tc000/ftb/bas2/bat1/bacfff/pa8/tt0/tw1/th1/ta1/tb4"
-				frameborder="0" width="207" height="58"></iframe>
-	</div>
+<!--	<div class="datum_tijd">-->
+<!--		<iframe src="https://free.timeanddate.com/clock/i80s3idm/n1297/tlaw14/fn6/fs16/fcfff/tc000/ftb/bas2/bat1/bacfff/pa8/tt0/tw1/th1/ta1/tb4"-->
+<!--				frameborder="0" width="207" height="58"></iframe>-->
+<!--	</div>-->
 
 	<div class="homeimg">
 
@@ -32,7 +32,7 @@ include_once($path);
 			</h2>
 			<a target="_blank"
 			   href="https://www.google.com/maps/place/Koestraat+1,+5223+AG+'s-Hertogenbosch/@51.697836,5.2876068,15.42z/data=!4m5!3m4!1s0x47c6ee66bc48c005:0x4dddc528356e3d4f!8m2!3d51.6973624!4d5.2935999">
-				<img src="<?php echo baseUrl() ?>/public/map.png" alt="map" a>
+				<img src="<?php echo baseUrl() ?>/public/map.png" alt="map">
 			</a>
 
 
