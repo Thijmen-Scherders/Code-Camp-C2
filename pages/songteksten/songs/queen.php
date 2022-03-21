6d3417f659dabfb4ed2f05e669ffef4e6d5bc08d
Wouterpiso <wpiso2000@gmail.com>
2022-03-21 12:48:09 +0100

    adding images to songteksten

--- pages/songteksten/songs/queen.php
+++ pages/songteksten/songs/queen.php
@@ -16,92 +16,97 @@
 <div class="wrapper">
 	<div class="container-column blue">
 		<h1>Bohemian Rhapsody </h1>
-		<p> 
-			
-            Is this the real life?
-            Is this just fantasy?
-            Caught in a landslide
-            No escape from reality
-		</p> 
-		<p> 
-            Open your eyes
-            Look up to the skies and see
-            I'm just a poor boy, I need no sympathy
-            Because I'm easy come, easy go
-            Little high, little low
-            Any way the wind blows
-            Doesn't really matter to me, to me
-		</p> 
-		<p> 
-            Mama, just killed a man
-            Put a gun against his head
-            Pulled my trigger, now he's dead
-            Mama, life had just begun
-            But now I've gone and thrown it all away
-		</p> 
-		<p> 
-            Mama, ooh
-            Didn't mean to make you cry
-            If I'm not back again this time tomorrow
-            Carry on, carry on as if nothing really matters
-		</p> 
-		<p> 
-            Too late, my time has come
-            Sends shivers down my spine
-            Body's aching all the time
-            Goodbye, everybody, I've got to go
-            Gotta leave you all behind and face the truth
-		</p> 
-		<p> 
-            Mama, ooh (Any way the wind blows)
-            I don't want to die
-            I sometimes wish I'd never been born at all
-		</p>
+        <div class="container song">
+            <img src="<?php echo baseUrl() ?>/public/Bohemian.png" alt="map"/>
+                <div class="container-column song">
+                    <p> 
+                        
+                        Is this the real life?
+                        Is this just fantasy?
+                        Caught in a landslide
+                        No escape from reality
+                    </p> 
+                    <p> 
+                        Open your eyes
+                        Look up to the skies and see
+                        I'm just a poor boy, I need no sympathy
+                        Because I'm easy come, easy go
+                        Little high, little low
+                        Any way the wind blows
+                        Doesn't really matter to me, to me
+                    </p> 
+                    <p> 
+                        Mama, just killed a man
+                        Put a gun against his head
+                        Pulled my trigger, now he's dead
+                        Mama, life had just begun
+                        But now I've gone and thrown it all away
+                    </p> 
+                    <p> 
+                        Mama, ooh
+                        Didn't mean to make you cry
+                        If I'm not back again this time tomorrow
+                        Carry on, carry on as if nothing really matters
+                    </p> 
+                    <p> 
+                        Too late, my time has come
+                        Sends shivers down my spine
+                        Body's aching all the time
+                        Goodbye, everybody, I've got to go
+                        Gotta leave you all behind and face the truth
+                    </p> 
+                    <p> 
+                        Mama, ooh (Any way the wind blows)
+                        I don't want to die
+                        I sometimes wish I'd never been born at all
+                    </p>
 
-        <p>
-            I see a little silhouetto of a man
-            Scaramouche, Scaramouche, will you do the Fandango?
-            Thunderbolt and lightning very, very frightening me
-            Galileo) Galileo
-            (Galileo) Galileo
-            Galileo Figaro
-            Magnifico-o-o-o-o
-        </p>
+                    <p>
+                        I see a little silhouetto of a man
+                        Scaramouche, Scaramouche, will you do the Fandango?
+                        Thunderbolt and lightning very, very frightening me
+                        Galileo) Galileo
+                        (Galileo) Galileo
+                        Galileo Figaro
+                        Magnifico-o-o-o-o
+                    </p>
 
-        <p>
-            I'm just a poor boy, nobody loves me
-            He's just a poor boy from a poor family
-            Spare him his life from this monstrosity
-        </p>
+                    <p>
+                        I'm just a poor boy, nobody loves me
+                        He's just a poor boy from a poor family
+                        Spare him his life from this monstrosity
+                    </p>
 
-        <p>
-            Easy come, easy go, will you let me go?
-            Bismillah! No, we will not let you go (Let him go!)
-            Bismillah! We will not let you go (Let him go!)
-            Bismillah! We will not let you go (Let me go!)
-            Will not let you go (Let me go!)
-            Never let you go (Never, never, never, never let me go)
-            Oh oh oh oh
-            No, no, no, no, no, no, no
-            Oh, mama mia, mama mia (Mama mia, let me go)
-            Beelzebub has a devil put aside for me, for me, for me
-        </p>
+                    <p>
+                        Easy come, easy go, will you let me go?
+                        Bismillah! No, we will not let you go (Let him go!)
+                        Bismillah! We will not let you go (Let him go!)
+                        Bismillah! We will not let you go (Let me go!)
+                        Will not let you go (Let me go!)
+                        Never let you go (Never, never, never, never let me go)
+                        Oh oh oh oh
+                        No, no, no, no, no, no, no
+                        Oh, mama mia, mama mia (Mama mia, let me go)
+                        Beelzebub has a devil put aside for me, for me, for me
+                    </p>
 
-        <p>
-            So you think you can stone me and spit in my eye?
-            So you think you can love me and leave me to die?
-            Oh, baby, can't do this to me, baby
-            Just gotta get out, just gotta get right outta here
-        </p>
+                    <p>
+                        So you think you can stone me and spit in my eye?
+                        So you think you can love me and leave me to die?
+                        Oh, baby, can't do this to me, baby
+                        Just gotta get out, just gotta get right outta here
+                    </p>
 
-        <p>
-            Ooooh, ooh yeah, ooh yeah
-            Nothing really matters
-            Anyone can see
-            Nothing really matters
-            Nothing really matters to me
-            Any way the wind blows...
-        </p>
+                    <p>
+                        Ooooh, ooh yeah, ooh yeah
+                        Nothing really matters
+                        Anyone can see
+                        Nothing really matters
+                        Nothing really matters to me
+                        Any way the wind blows...
+                    </p>
+                </div>
+        </div>
 	</div>
 </div>
 
