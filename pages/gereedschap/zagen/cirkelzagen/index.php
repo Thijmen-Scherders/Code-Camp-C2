98d4e67a5674fd5a9ff20d39b8f7ab52cddf0aa7
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-03-21 13:22:24 +0100

    de basis header footer aan cirkelzaag
    pagina header footer toegevoegd

--- pages/gereedschap/zagen/cirkelzagen/index.php
+++ pages/gereedschap/zagen/cirkelzagen/index.php
@@ -1,15 +1,38 @@
-<h1>cirkelzagen</h1>
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/backend/config.php";
+include_once($path);
+?>
 
-<h2>Een cirkelzaag (of radiaalzaag) is een zaag met een blad in de vorm van 
-    een stalen schijf, die met een motor tot een zeer hoog toerental wordt 
-    aangedreven. De schijf kan in een zaagtafel zijn gemonteerd waarover het 
-    te zagen materiaal wordt aangevoerd, of ingebouwd in een constructie samen 
-    met een elektromotor tot draagbare handcirkelzaag. Deze heeft een minder 
-    krachtige motor en kan ook dunner materiaal zagen. Cirkelzaagbladen bestaan
-    in soorten en maten, met allerlei verschillende vertandingen, en ook de 
-    diameter kan sterk verschillen (normaal is ca. 25 cm, maar industriële 
-    cirkelzagen met veel grotere bladen bestaan ook). De maximale zaagdiepte 
-    wordt bepaald door de straal van het cirkelzaagblad. Een cirkelzaag die 
-    bedoeld is om latten, buizen, houten balken en dergelijke te zagen wordt 
-    ook wel een afkorter of afkortzaag genoemd.
-</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="<?php echo baseUrl() ?>/public/stylesheet.css">
+	<title>Songteksten</title>
+</head>
+
+<body class="mainpage">
+
+<?php includeHeader(); ?>   
+    <div class="cirkelbox">
+        <h1>cirkelzagen</h1>
+        <img src="/public/cirkelzaag.jpg" alt="cirkelzaag">
+        <h2>Een cirkelzaag (of radiaalzaag) is een zaag met een blad in de vorm van 
+            een stalen schijf, die met een motor tot een zeer hoog toerental wordt 
+            aangedreven. De schijf kan in een zaagtafel zijn gemonteerd waarover het 
+            te zagen materiaal wordt aangevoerd, of ingebouwd in een constructie samen 
+            met een elektromotor tot draagbare handcirkelzaag. Deze heeft een minder 
+            krachtige motor en kan ook dunner materiaal zagen. Cirkelzaagbladen bestaan
+            in soorten en maten, met allerlei verschillende vertandingen, en ook de 
+            diameter kan sterk verschillen (normaal is ca. 25 cm, maar industriële 
+            cirkelzagen met veel grotere bladen bestaan ook). De maximale zaagdiepte 
+            wordt bepaald door de straal van het cirkelzaagblad. Een cirkelzaag die 
+            bedoeld is om latten, buizen, houten balken en dergelijke te zagen wordt 
+            ook wel een afkorter of afkortzaag genoemd.
+        </h2>
+    </div>
+    <?php includeFooter(); ?>
+
+</body>
+
+</html>
