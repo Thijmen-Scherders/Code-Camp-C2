1f14f1ab5a6ba05a4c0d0d4cdbf166a606614018
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-03-21 14:39:58 +0100

    plaatjes toegevoegd schroevendraaiers
    afbeeldingen zijn toegevoegd

--- pages/gereedschap/schroevendraaiers/index.php
+++ pages/gereedschap/schroevendraaiers/index.php
@@ -21,7 +21,14 @@ include_once($path);
             <h1>schroevendraaiers</h1>
             <h2>Een schroevendraaier is een stuk gereedschap waarmee schroeven worden aangebracht en verwijderd. Het bestaat uit een handvat met een metalen steel waarvan het uiteinde zo gevormd is dat het in de kop van een schroef past.
                 Schroevendraaiers zijn in velerlei soorten, afmetingen en kwaliteiten verkrijgbaar. Er bestaan schroevendraaiers met een platte punt, alsook met een ster- of kruiskop. Goede schroevendraaiers zijn vervaardigd van speciale staalsoorten. Bij een juiste toepassing zijn ze praktisch onbreekbaar. Het heft van een schroevendraaier wordt meestal gemaakt van een slagvaste, onbreekbare kunststof. Soms vervaardigt men het heft nog van een taaie houtsoort. De grootte van schroevendraaier en schroef dienen bij elkaar te 'passen'.</h2>
-            <h2>sleufkopschroevendraaier (plat);</h2>
+            <div class="order1">
+                <img src="/public/img/sleuf.jpg" alt="sleufkopschroevendraaier">
+                <h2>sleufkopschroevendraaier (plat);</h2>
+                <img src="/public/img/phillip.jpg" alt="phillipsschroevendraaier">
+                <h2>phillipsschroevendraaier PH (kruiskop zonder verdere markering);</h2>
+                <img src="/public/img/pozid.jpg" alt="pozidrivschroevendraaier">
+                <h2>pozidrivschroevendraaier PZ (kruiskop met extra sterretje).</h2>
+            </div>
         </div>
 
     <?php includeFooter(); ?>
