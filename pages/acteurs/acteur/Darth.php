3dd7c7236c4fc56510d00d4139f09953283bdab0
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-03-21 13:05:10 +0100

    text wat duidelijker

--- pages/acteurs/acteur/Darth.php
+++ pages/acteurs/acteur/Darth.php
@@ -21,22 +21,26 @@ include_once($path);
 <div class="wrapper yellow">
 	<div class="container black">
 		<img src="<?php echo baseUrl() ?>/public/darth.jpg" alt="map"/>
-		<p class="darth">
-			Darth Vader is a fictional character in the Star Wars franchise. The character is the primary antagonist in
-			the original trilogy and, as Anakin Skywalker, is a primary protagonist in the prequel trilogy. Star Wars
-			creator George Lucas has collectively referred to the first six episodic films of the franchise as "the
-			tragedy of Darth Vader".[3] He has become one of the most iconic villains in popular culture, and has been
-			listed among the greatest villains and fictional characters ever.[4][5]
+		<div class="container-column">
+			<p class="darth">
+				Darth Vader is a fictional character in the Star Wars franchise. The character is the primary antagonist in
+				the original trilogy and, as Anakin Skywalker, is a primary protagonist in the prequel trilogy. Star Wars
+				creator George Lucas has collectively referred to the first six episodic films of the franchise as "the
+				tragedy of Darth Vader".[3] He has become one of the most iconic villains in popular culture, and has been
+				listed among the greatest villains and fictional characters ever.[4][5]
+			</p>
+				
+			<p class="darth">
 
-			Originally a slave on Tatooine, Anakin Skywalker is a Jedi prophesied to bring balance to the Force. He is
-			lured to the dark side of the Force by Palpatine and becomes a Sith Lord, assuming the title Darth Vader.
-			After a lightsaber battle with his former mentor Obi-Wan Kenobi on Mustafar, in which he is severely
-			injured, Vader is transformed into a cyborg. He then serves the Galactic Empire as its chief enforcer. Vader
-			ultimately redeems himself by saving his son, Luke Skywalker, and killing Palpatine, sacrificing his own
-			life in the process.[6] He is also the secret husband of Padmé Amidala, biological father of Princess Leia,
-			and maternal grandfather of Kylo Ren.
-		</p>
-
+				Originally a slave on Tatooine, Anakin Skywalker is a Jedi prophesied to bring balance to the Force. He is
+				lured to the dark side of the Force by Palpatine and becomes a Sith Lord, assuming the title Darth Vader.
+				After a lightsaber battle with his former mentor Obi-Wan Kenobi on Mustafar, in which he is severely
+				injured, Vader is transformed into a cyborg. He then serves the Galactic Empire as its chief enforcer. Vader
+				ultimately redeems himself by saving his son, Luke Skywalker, and killing Palpatine, sacrificing his own
+				life in the process.[6] He is also the secret husband of Padmé Amidala, biological father of Princess Leia,
+				and maternal grandfather of Kylo Ren.
+			</p>
+		</div>
 
 	</div>
 </div>
