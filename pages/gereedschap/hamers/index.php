f7178f68e4b1a1eb42f28c5e85b1cd9314b3e268
Natebuilder <75074109+Natebuilder@users.noreply.github.com>
2022-03-22 15:03:47 +0100

    een div class kopje aangemaakt
    div class kopje aangemaakt voor de kopjes van de hamer pagina

--- pages/gereedschap/hamers/index.php
+++ pages/gereedschap/hamers/index.php
@@ -34,43 +34,42 @@ include_once($path);
 			<h2>
 				De volgende tabel geeft een overzicht van typen van hamers:
 			</h2>
-
-			<h2>gewone hamer
-
+			
+			<div class="kopje"><h2>gewone hamer</h2></div>
+			<h2>
 				Een metalen hamerkop van 50-300 gram op een houten steel van ca.
 				30 cm lang die onder meer wordt gebruikt om spijkers (draadnagels)
 				in houten voorwerpen te slaan.</h2>
 
-			<h2>klauwhamer
-
+			<div class="kopje"><h2>klauwhamer</h2></div>
+			<h2>
 				Zoals een gewone hamer, maar de achterkant van de hamer heeft een
 				platte V-groef waarin spijkers geklemd kunnen worden. De bovenkant
 				is afgerond zodat de geklemde spijker uit het object gerold kan worden.</h2>
 
-			<h2>vuist of moker
-
+			<div class="kopje"><h2>vuist of moker</h2></div>
+			<h2>
 				Een zware hamer met een vierkante hamerkop van 1000-1500 gram op een
 				steel van 30 cm lang, voor zwaar werk. Met een rubberen opzetstuk wordt
 				een vuisthamer gebruikt om tegels of straatstenen in het zand te leggen
 				voor het maken van bestrating.</h2>
 
-			<h2>tegelhamer
-
+			<div class="kopje"><h2>tegelhamer</h2></div>
+			<h2>
 				Houten hamer met grote houten kop en een korte steel. Om tegels te leggen.</h2>
 
-			<h2>straathamer
-
+			<div class="kopje"><h2>straathamer</h2></div>
+			<h2>
 				Een hamer om te straten, met een kromme zijde om stenen door midden te
 				slaan, en een rechte afgeplatte zijde om stenen aan te slaan (bestraten).</h2>
 
-			<h2>voorhamer
-
+			<div class="kopje"><h2>voorhamer</h2></div>
+			<h2>
 				Een hamer om sloopwerkzaamheden uit te voeren. Een hamerkop van een paar
 				kilo op een steel van een meter lang.</h2>
 
-			<h2>rubberen hamer
-
-				Een rubberen hamer wordt vooral gebruikt bij het leggen van tegels op grond.
+			<div class="kopje"><h2>rubberen hamer</h2></div>
+			<h2>Een rubberen hamer wordt vooral gebruikt bij het leggen van tegels op grond.
 				Een kleine hamer met een rubberen gewicht die kan worden gebruikt voor
 				voorzichtig werk, zoals het aankloppen van kleine lijmverbindingen in de
 				houtbewerking. Kampeerders gebruiken een rubber hamer om de tentpinnen in
