c4fe0cd062e5b72a81329c9437b14bd5645c9964
FerryDeVries <71122367+FerryDeVries@users.noreply.github.com>
2022-03-23 09:58:24 +0100

    Added page george clooney in sub menu

--- pages/acteurs/submenu.php
+++ pages/acteurs/submenu.php
@@ -10,5 +10,6 @@ include_once($path);
         <a href="<?php echo baseUrl(); ?>/pages/acteurs/acteur/Timothy-Dalton.php">Timothy Dalton</a>
         <a href="<?php echo baseUrl(); ?>/pages/acteurs/acteur/Darth.php">Darth Vader</a>
         <a href="<?php echo baseUrl(); ?>/pages/acteurs/acteur/luke.php">Luke Skywalker</a>
+         <a href="<?php echo baseUrl(); ?>/pages/acteurs/acteur/Georgeclooney.php">George Clooney</a>
     </div>
 </div>
