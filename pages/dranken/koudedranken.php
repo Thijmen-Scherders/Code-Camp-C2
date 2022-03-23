23682d283a63b625161c40466269f4f33377114e
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-03-23 10:46:56 +0100

    koude drank pagina alleen kleur er nog bij

--- pages/dranken/koudedranken.php
+++ pages/dranken/koudedranken.php
@@ -12,56 +12,33 @@ include_once($path);
 	<link rel="stylesheet" href="<?php echo baseUrl() ?>/public/stylesheet.css">
 	<title>Koude dranken</title>
 </head>
-<body class="mainpage">
+<body class="mainpage bg-orange">
 
 <?php includeHeader(); ?>
 
 <?php includeDrinksSubMenu(); ?>
 
-<div class="wrapper">
-	<div class="container orange">
-		<div class="wrapperfris">
-			<div class="container1">
-			<h1>Cola</h1>
-				<div class="container">
-					<img src="/public/img/cola.jpg" alt="cola" width="300px" height="300px">
-					<div class="container-column">
-						<p> Cola is een koolzuurhoudende frisdrank. Het smaakt zoet (maar is qua pH zuur) en bevat circa 28-32 mg cafeïne per 33 cl. Andere belangrijke ingrediënten zijn vanille, kaneel, karamel en citroen</p>
-						<p>Anders dan de meeste frisdranken bevat het geen citroenzuur, maar fosforzuur. Dieetcola bevat zowel fosforzuur als citroenzuur. Er bestaan naast suikervrije ook cafeïnevrije colavarianten.</p>
-					</div>
-				</div>
+<div class="container-row">
+		<div class="container-column koffie bg-grey">
+			<h2>Cola</h2>
+			<img src="/public/img/cola.jpg" alt="cola" width="300px" height="300px">
+			<p>Cola is een koolzuurhoudende frisdrank. Het smaakt zoet (maar is qua pH zuur) en bevat circa 28-32 mg cafeïne per 33 cl. Andere belangrijke ingrediënten zijn vanille, kaneel, karamel en citroen.</p>
+			<div class="link-fix">
+			<a href="<?php echo baseUrl() ?>/pages/acteurs/acteur/Roger-Moore.php">Roger Moore</a>
 			</div>
-			<div class="container">
-				<a href="<?php echo baseUrl() ?>/pages/acteurs/acteur/Roger-Moore.php">Roger Moore</a>
-			</div>
-
-			<div class="container1">
-				<h1>7-Up</h1>
-
-				<div class="container">
-					<img src="/public/img/up.jpg" alt="up" width="300px" height="300px">
-					<div class="container-column">
-						<p> 7 Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group in de Verenigde Staten en PepsiCo in de rest van de wereld</p>
-						<p>7 Up (gestileerd als 7up buiten de VS) is een Amerikaans merk van niet-cafeïnehoudende frisdrank met citroen-limoensmaak. De rechten op het merk zijn in handen van Keurig Dr Pepper in de Verenigde Staten en van 7 Up international in de rest van de wereld. De Amerikaanse versie van het 7 Up-logo bevat een roodoranje cirkel tussen de "7" en "Up"; deze rood-oranje cirkel is geanimeerd en gebruikt als mascotte voor het merk als Cool Spot. Daarvoor was de mascotte een fictief personage</p>
-					</div>
-				</div>
-			</div>
-			<div class="container1">
-			<h1>Fanta</h1>
-
-				<div class="container">
-					<img src="/public/img/fanta.jpg" alt="sinas" width="300px" height="300px">
-					<div class="container-column">
-						<p>Fanta is in Nederland de benaming voor de doorgaans gele of oranje, meestal koolzuurhoudende frisdrank met sinaasappelsmaak. Zoals de meeste frisdranken bevat sinas veel suiker of in de light-versie kunstmatige zoetstoffen.</p>
-						<p>Fanta is een koolzuurhoudende frisdrank van The Coca-Cola Company. Fanta is vooral bekend als een sinaasappeldrank met koolzuur (Fanta Orange), maar er bestaan tal van andere smaken, al dan niet met koolzuur. Te noemen zijn onder andere: Mandarin, Fruit Twist, Lemon, Skwizz, Shokata, Apple, Strawberry en Pink Kiss. Veel van deze smaken zijn ook in light-versie beschikbaar. In Nederland kent Fanta de volgende smaken: Orange, Pomelo, Red Fruit, Cassis, Exotic, Lemon en Mezzo.</p>
-					</div>
-				</div>
-			</div>
-
 		</div>
-</div>
-</div>
-
+		<div class="container-column latte bg-grey">
+			<h2>7-up</h2>
+			<img src="/public/img/up.jpg" alt="up" width="300px" height="300px">
+			<p>7 Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group in de Verenigde Staten en PepsiCo in de rest van de wereld.</p>
+		</div>
+		<div class="container-column thee bg-grey">
+			<h2>Fanta</h2>
+			<img src="/public/img/fanta.jpg" alt="sinas" width="300px" height="300px">
+			<p>Fanta is een koolzuurhoudende frisdrank van The Coca-Cola Company. Fanta is vooral bekend als een sinaasappeldrank met koolzuur (Fanta Orange), maar er bestaan tal van andere smaken, al dan niet met koolzuur. Te noemen zijn onder andere: Mandarin, Fruit Twist, Lemon, Skwizz, Shokata, Apple, Strawberry en Pink Kiss. </p>
+			
+		</div>
+	</div>
 <?php includeFooter(); ?>
 
 </body>
