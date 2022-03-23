b8a25814e252308574f41961d0256a28c5e0fc97
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-03-23 09:00:28 +0100

    tekst cirkelzagen meer uit elkaar
    Foto moet wel weer aangepast worden

--- pages/gereedschap/tool/zagen/cirkelzagen.php
+++ pages/gereedschap/tool/zagen/cirkelzagen.php
@@ -19,20 +19,24 @@ include_once($path);
 
     <div class="cirkelbox">
         <h1>cirkelzagen</h1>
-        <img src="/public/img/cirkelzaag.jpg" alt="cirkelzaag">
-        <h2>Een cirkelzaag (of radiaalzaag) is een zaag met een blad in de vorm van
-            een stalen schijf, die met een motor tot een zeer hoog toerental wordt
-            aangedreven. De schijf kan in een zaagtafel zijn gemonteerd waarover het
-            te zagen materiaal wordt aangevoerd, of ingebouwd in een constructie samen
-            met een elektromotor tot draagbare handcirkelzaag. Deze heeft een minder
-            krachtige motor en kan ook dunner materiaal zagen. Cirkelzaagbladen bestaan
+        <div class="container-column">
+            <img src="/public/img/cirkelzaag.jpg" alt="cirkelzaag">
+            <p>Een cirkelzaag (of radiaalzaag) is een zaag met een blad in de vorm van
+                een stalen schijf, die met een motor tot een zeer hoog toerental wordt
+                aangedreven. De schijf kan in een zaagtafel zijn gemonteerd waarover het
+                te zagen materiaal wordt aangevoerd, of ingebouwd in een constructie samen
+                met een elektromotor tot draagbare handcirkelzaag. Deze heeft een minder
+                krachtige motor en kan ook dunner materiaal zagen.
+            </p> 
+            <p>Cirkelzaagbladen bestaan
             in soorten en maten, met allerlei verschillende vertandingen, en ook de
             diameter kan sterk verschillen (normaal is ca. 25 cm, maar industriële
             cirkelzagen met veel grotere bladen bestaan ook). De maximale zaagdiepte
             wordt bepaald door de straal van het cirkelzaagblad. Een cirkelzaag die
             bedoeld is om latten, buizen, houten balken en dergelijke te zagen wordt
             ook wel een afkorter of afkortzaag genoemd.
-        </h2>
+             </p>
+        </div>
     </div>
     <?php includeFooter(); ?>
 
