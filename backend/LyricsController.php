9dc58302a224fae59a316d1bafcd65a556e68fb6
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-28 12:14:21 +0200

    Add lyrics and user controllers

--- backend/LyricsController.php
+++ backend/LyricsController.php
@@ -1,1 +1,52 @@
 <?php
+
+function getAllLyrics()
+{
+    require_once 'conn.php';
+    $query = "SELECT * FROM lyrics";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function createNewLyrics($title, $text)
+{
+    require_once 'conn.php';
+    try {
+        $query = "INSERT INTO lyrics (title, text) VALUES ('$title', '$text')";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        echo $statement->rowCount() . " records UPDATED successfully";
+    } catch (PDOException $ex) {
+        return $ex;
+    }
+    return false;
+}
+
+function updateLyricsById($id, $title, $text)
+{
+    require_once 'conn.php';
+    try {
+        $query = "UPDATE lyrics SET title='$title', text='$text' WHERE id='$id'";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        echo $statement->rowCount() . " records UPDATED successfully";
+    } catch (PDOException $ex) {
+        return $ex;
+    }
+    return false;
+}
+
+function deleteLyricsById($id)
+{
+    try {
+        require_once 'conn.php';
+        $query = "DELETE FROM lyrics WHERE id = '$id'";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        echo $statement->rowCount() . " records UPDATED successfully";
+    } catch (PDOException $ex) {
+        return $ex;
+    }
+    return false;
+}
