905a93d5993d91b2a816f7d5ce3670a261c9660c
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-03-28 13:01:01 +0200

    schroevendraaier gefixed

--- pages/gereedschap/tool/schroevendraaiers.php
+++ pages/gereedschap/tool/schroevendraaiers.php
@@ -19,13 +19,25 @@ include_once($path);
 
 <div class="schroevendraaier">
 	<h1>schroevendraaiers</h1>
-	<h2>Een schroevendraaier is een stuk gereedschap waarmee schroeven worden aangebracht en verwijderd. Het bestaat uit
-		een handvat met een metalen steel waarvan het uiteinde zo gevormd is dat het in de kop van een schroef past.
-		Schroevendraaiers zijn in velerlei soorten, afmetingen en kwaliteiten verkrijgbaar. Er bestaan schroevendraaiers
-		met een platte punt, alsook met een ster- of kruiskop. Goede schroevendraaiers zijn vervaardigd van speciale
-		staalsoorten. Bij een juiste toepassing zijn ze praktisch onbreekbaar. Het heft van een schroevendraaier wordt
-		meestal gemaakt van een slagvaste, onbreekbare kunststof. Soms vervaardigt men het heft nog van een taaie
-		houtsoort. De grootte van schroevendraaier en schroef dienen bij elkaar te 'passen'.</h2>
+	
+	<div class="container-column">
+		
+		<p>Een schroevendraaier is een stuk gereedschap waarmee schroeven worden aangebracht en verwijderd. Het bestaat uit
+			een handvat met een metalen steel waarvan het uiteinde zo gevormd is dat het in de kop van een schroef past.
+			Schroevendraaiers zijn in velerlei soorten, afmetingen en kwaliteiten verkrijgbaar. Er bestaan schroevendraaiers
+			met een platte punt, alsook met een ster- of kruiskop.
+		</p>
+		
+		<p>
+			Goede schroevendraaiers zijn vervaardigd van speciale
+			staalsoorten. Bij een juiste toepassing zijn ze praktisch onbreekbaar. Het heft van een schroevendraaier wordt
+			meestal gemaakt van een slagvaste, onbreekbare kunststof. Soms vervaardigt men het heft nog van een taaie
+			houtsoort. De grootte van schroevendraaier en schroef dienen bij elkaar te 'passen'.
+		</p>
+		
+	</div>
+
+	
 	<div class="order1">
 		<img src="/public/img/sleuf.jpg" alt="sleufkopschroevendraaier">
 		<h2>sleufkopschroevendraaier (plat);</h2>
