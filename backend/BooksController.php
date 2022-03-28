4a8bdead15c9d6de9582cd9addb2eb948309cffe
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-28 11:59:00 +0200

    Add create books method

--- backend/BooksController.php
+++ backend/BooksController.php
@@ -7,3 +7,13 @@ function getAllBooks() {
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function createNewBook($title, $author, $description, $pages, $price): bool
+{
+    require_once 'conn.php';
+
+    $query = "INSERT INTO books (title, author, description, pages, price) VALUES ('$title', '$author', '$description', '$pages', '$price')";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    return true;
+}
