f5236e2aa59e248385f8ce25879d92c5f553c6f2
Wouterpiso <wpiso2000@gmail.com>
2022-03-29 15:29:06 +0200

    added land page

---
+++ pages/acteurs/land.php
@@ -0,0 +1,28 @@
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= "/backend/config.php";
+include_once($path);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="<?php echo baseUrl() ?>/public/stylesheet.css">
+	<title>Film Personages</title>
+</head>
+
+<body class="mainpage">
+
+<?php includeHeader(); ?>
+
+<?php includeFilmSubMenu(); ?>
+
+<div class="kruimelpad">
+	Kruimelpad - <a href="<?php echo baseUrl() ?>/pages/acteurs/index.php">Acteurs</a>
+</div>
+
+<?php includeFooter(); ?>
+
+</body>
+
+</html>
--- pages/acteurs/submenu.php
+++ pages/acteurs/submenu.php
@@ -9,6 +9,6 @@ include_once($path);
         <a href="<?php echo baseUrl(); ?>/pages/acteurs/acteur/Roger-Moore/Roger-Moore.php">Roger Moore</a>
         <a href="<?php echo baseUrl(); ?>/pages/acteurs/acteur/Timothy-Dalton/Timothy-Dalton.php">Timothy Dalton</a>
         <a href="<?php echo baseUrl(); ?>/pages/acteurs/acteur/George-clooney/Georgeclooney.php">George Clooney</a>
-        <a href="<?php echo baseUrl(); ?>/pages/acteurs/acteur/Darth.php">Film Personages</a>
+        <a href="<?php echo baseUrl(); ?>/pages/acteurs/land.php">Film Personages</a>
     </div>
 </div>
