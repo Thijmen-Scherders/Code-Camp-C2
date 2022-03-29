143223d085f995df8afca52dd3a17e0dbd0de996
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-29 16:09:38 +0200

    Cleanup

--- pages/boeken/edit/book.php
+++ pages/boeken/edit/book.php
@@ -4,12 +4,9 @@ $config = $path . "/backend/config.php";
 include_once($config);
 include_once($path . "/backend/BooksController.php");
 
-
 $url= $_SERVER['REQUEST_URI'];
-//echo $url;
 $parts = parse_url($url);
 parse_str($parts['query'], $query);
-//echo $query['id'];
 $boek = getBookById($query['id']);
 ?>
 
