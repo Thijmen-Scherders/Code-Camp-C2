dd5c40b6298a97bf3de4dfcf84b1a210eb1fa859
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-29 16:09:29 +0200

    Get book by id

--- backend/BooksController.php
+++ backend/BooksController.php
@@ -9,6 +9,14 @@ function getAllBooks()
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function getBookById($id) {
+    require_once 'conn.php';
+    $query = "SELECT title, author, description, pages, price, image_url  FROM books WHERE id='$id'";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
 function createNewBook($title, $author, $description, $pages, $price)
 {
     require_once 'conn.php';
