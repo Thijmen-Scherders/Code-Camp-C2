4d48e54ecbaccb636f1f4bde2d5267dfaec15e32
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-30 11:45:37 +0200

    Add delete method with confirmation

---
+++ pages/boeken/edit/delete.php
@@ -0,0 +1,14 @@
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$config = $path."/backend/config.php";
+include_once($config);
+include_once($path."/backend/BooksController.php");
+
+$url = $_SERVER['REQUEST_URI'];
+$parts = parse_url($url);
+parse_str($parts['query'], $query);
+
+deleteBookById($query['id']);
+
+header("Location: ".baseUrl()."/pages/boeken/edit/overview.php");
+exit();
