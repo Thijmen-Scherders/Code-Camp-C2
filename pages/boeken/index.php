9421b3950ce796ffc89977104d749eb23c6bce6d
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-30 12:24:59 +0200

    Update books page to new format

--- pages/boeken/index.php
+++ pages/boeken/index.php
@@ -1,54 +1,35 @@
 <?php
 $path = $_SERVER['DOCUMENT_ROOT'];
-$path .= "/backend/config.php";
-include_once($path);
+$config = $path . "/backend/config.php";
+include_once($config);
+include_once($path . "/backend/BooksController.php");
+$books = getAllBooks()
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<link rel="stylesheet" href="<?php echo baseUrl() ?>/public/stylesheet.css">
+	<link rel="stylesheet" href="<?php echo baseUrl() ?>/public/books.css">
 	<title>Boeken</title>
 </head>
 
-<body class="mainpage">
-
 <?php includeHeader(); ?>
 
-<?php includeBooksSubMenu(); ?>
+<div class="overview">
+	<div class="books">
+        <?php foreach ($books as $book): ?>
+			<div class="book">
+				<img class="image" src="<?php echo $book['image_url'] ?>" alt="book image"/>
+				<div class="details">
+					<p class="title"><?php echo $book['title']; ?></p>
+					<p class="author">Author: <?php echo $book['author']; ?></p>
+					<p class="description">Description: <?php echo $book['description']; ?></p>
+					<p class="pages">Pages: <?php echo $book['pages']; ?></p>
+					<p class="price">Price: $<?php echo $book['price']; ?></p>
 
-<div class="wrapper">
-	<div class="container-column black">
-		<h1>Boeken</h1>
-	<div class="container-column">
-		<p>
-			Het boek is een veelgebruikte manier om informatie vast te leggen 
-			in de vorm van tekst en afbeeldingen en om deze informatie weer te 
-			geven, op te slaan en te verspreiden. Het is een document van vaak 
-			niet al te kleine omvang. Een boek in traditionele zin is een 
-			samengevoegde hoeveelheid papier met tekst, afbeeldingen of allebei. 
-			Daarnaast wordt met de term de betreffende informatie, of een 
-			soortgelijke verzameling informatie, aangeduid ('een boek schrijven', 
-			'een boek downloaden'). 
-		</p>
-
-		<p>Het woord 'boek' is vermoedelijk afkomstig van 
-			het Germaanse woord voor beuk, boche. De omslagen van de eerste boeken 
-			werden namelijk gemaakt van een rechthoekig stuk (beuken)hout. Het 
-			Griekse woord biblia (boeken) is het meervoud van biblion dat 
-			oorspronkelijk 'papyrusje' betekende, het materiaal waarop in de 
-			oudheid meestal werd geschreven, en later werd gebruikt als aanduiding 
-			van papier, geschrift en boek(rol).
-		</p>
+				</div>
+			</div>
+        <?php endforeach; ?>
 	</div>
 </div>
-
-	<div class="kruimelpad">
-		Kruimelpad - <a href="<?php echo baseUrl() ?>/pages/boeken/index.php">Boeken</a> / Overzicht
-	</div>
-
-<?php includeFooter(); ?>
-
-</body>
-
-</html>
