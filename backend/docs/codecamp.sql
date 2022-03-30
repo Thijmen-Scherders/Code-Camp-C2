b34a662bc9cc9c237350d284dc90b8ca37d2bd2d
Laurant Marijnissen <laurant@gigafyde.dev>
2022-03-30 12:37:13 +0200

    Update sql export

--- backend/docs/codecamp.sql
+++ backend/docs/codecamp.sql
@@ -17,6 +17,7 @@ CREATE DATABASE IF NOT EXISTS `codecamp-c2` /*!40100 DEFAULT CHARACTER SET latin
 USE `codecamp-c2`;
 
 -- Structuur van  tabel codecamp-c2.books wordt geschreven
+DROP TABLE IF EXISTS `books`;
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
@@ -25,19 +26,21 @@ CREATE TABLE IF NOT EXISTS `books` (
   `pages` int(11) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `image_url` varchar(255) DEFAULT NULL,
+  `likes` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 -- Dumpen data van tabel codecamp-c2.books: ~4 rows (ongeveer)
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` (`id`, `title`, `author`, `description`, `pages`, `price`, `image_url`) VALUES
-	(8, 'De vlinder', 'Rups', '', 1, 1, 'https://code-camp-c2.test/public/books/7bb287ae-3193-4a85-bf64-4ab5bce5410a.jpeg'),
-	(15, 'Steen', 'Henk', 'Het vervolg van het vervolg', 1, 1, 'https://code-camp-c2.test/public/books/7bb287ae-3193-4a85-bf64-4ab5bce5410a.jpeg'),
-	(16, 'Steen', 'Henk', 'Het vervolg van het vervolg', 1, 1, 'https://code-camp-c2.test/public/books/7bb287ae-3193-4a85-bf64-4ab5bce5410a.jpeg'),
-	(17, 'Doos', 'Doos', 'Doos', 1, 1, 'https://code-camp-c2.test/public/books/7bb287ae-3193-4a85-bf64-4ab5bce5410a.jpeg');
+INSERT INTO `books` (`id`, `title`, `author`, `description`, `pages`, `price`, `image_url`, `likes`) VALUES
+	(8, 'De vlinder', 'Rups', '', 1, 1, 'https://code-camp-c2.test/public/books/7bb287ae-3193-4a85-bf64-4ab5bce5410a.jpeg', 1),
+	(15, 'Steen', 'Henk', 'Het vervolg van het vervolg', 1, 1, 'https://code-camp-c2.test/public/books/7bb287ae-3193-4a85-bf64-4ab5bce5410a.jpeg', 2),
+	(16, 'Steen', 'Henk', 'Het vervolg van het vervolg', 1, 1, 'https://code-camp-c2.test/public/books/7bb287ae-3193-4a85-bf64-4ab5bce5410a.jpeg', 3),
+	(17, 'Doos', 'Doos', 'Doos', 1, 1, 'https://code-camp-c2.test/public/books/7bb287ae-3193-4a85-bf64-4ab5bce5410a.jpeg', 4);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 
 -- Structuur van  tabel codecamp-c2.lyrics wordt geschreven
+DROP TABLE IF EXISTS `lyrics`;
 CREATE TABLE IF NOT EXISTS `lyrics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
@@ -54,6 +57,7 @@ CREATE TABLE IF NOT EXISTS `lyrics` (
 /*!40000 ALTER TABLE `lyrics` ENABLE KEYS */;
 
 -- Structuur van  tabel codecamp-c2.users wordt geschreven
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL,
@@ -65,28 +69,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumpen data van tabel codecamp-c2.users: ~0 rows (ongeveer)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
--- Structuur van  tabel codecamp-c2.user_book_likes wordt geschreven
-CREATE TABLE IF NOT EXISTS `user_book_likes` (
-  `user_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumpen data van tabel codecamp-c2.user_book_likes: ~0 rows (ongeveer)
-/*!40000 ALTER TABLE `user_book_likes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_book_likes` ENABLE KEYS */;
-
--- Structuur van  tabel codecamp-c2.user_lyrics_likes wordt geschreven
-CREATE TABLE IF NOT EXISTS `user_lyrics_likes` (
-  `user_id` int(11) NOT NULL,
-  `lyrics_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumpen data van tabel codecamp-c2.user_lyrics_likes: ~0 rows (ongeveer)
-/*!40000 ALTER TABLE `user_lyrics_likes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_lyrics_likes` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
