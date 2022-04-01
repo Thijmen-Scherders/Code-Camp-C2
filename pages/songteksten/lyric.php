70a1e10b87e1b751337ea452865059f800d05693
Laurant Marijnissen <laurant@gigafyde.dev>
2022-04-01 19:09:08 +0200

    Fix styling

--- pages/songteksten/lyric.php
+++ pages/songteksten/lyric.php
@@ -10,9 +10,9 @@ parse_str($parts['query'], $query);
 $lyrics = getAllLyrics();
 $lyricDetail = null;
 foreach ($lyrics as $lyric):
-	if ($query['id'] === $lyric['id']) {
-		$lyricDetail = $lyric;
-	}
+    if ($query['id'] === $lyric['id']) {
+        $lyricDetail = $lyric;
+    }
 endforeach;
 ?>
 
@@ -20,29 +20,33 @@ endforeach;
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="<?php echo baseUrl() ?>/public/stylesheet.css">
-    <link rel="stylesheet" href="<?php echo baseUrl() ?>/public/lyrics.css">
-    <title><?php echo $lyricDetail['title']?> - <?php echo $lyricDetail['author']?></title>
+	<link rel="stylesheet" href="<?php echo baseUrl() ?>/public/stylesheet.css">
+<!--	<link rel="stylesheet" href="--><?php //echo baseUrl() ?><!--/public/lyrics.css">-->
+	<title><?php echo $lyricDetail['title'] ?> - <?php echo $lyricDetail['author'] ?></title>
 </head>
 
 <?php includeHeader(); ?>
 
 <div class="wrapper">
-	<div class="container black">
-        <?php foreach ($lyrics as $lyric): ?>
+	<div class="container-column blue">
+		<div class="container song">
+            <?php foreach ($lyrics as $lyric): ?>
 
-			<a href="<?php echo baseUrl() ?>/pages/songteksten/lyric.php?id=<?php echo $lyric['id'] ?>"><?php echo $lyric['title'] ?> - <?php echo $lyric['author'] ?></a>
-        <?php endforeach; ?>
+				<a href="<?php echo baseUrl() ?>/pages/songteksten/lyric.php?id=<?php echo $lyric['id'] ?>"><?php echo $lyric['title'] ?>
+					- <?php echo $lyric['author'] ?></a>
+            <?php endforeach; ?>
+		</div>
 	</div>
 </div>
 
 <div class="wrapper">
-    <div class="container-column <?php echo $lyricDetail['background_color']?>">
-        <h1><?php echo $lyricDetail['title']?> - <?php echo $lyricDetail['author']?></h1>
-        <div class="container song">
+	<div class="container-column <?php echo $lyricDetail['background_color'] ?>">
+		<h1><?php echo $lyricDetail['title'] ?> - <?php echo $lyricDetail['author'] ?></h1>
+		<div class="container song">
 
-            <img class="image" src="<?php echo $lyricDetail['image_url'] ?>" alt="book image"/>
-            <p><?php echo $lyricDetail['text']?></p>
-
-    </div>
-</div>
+			<img class="image" src="<?php echo $lyricDetail['image_url'] ?>" alt="book image"/>
+			<div class="container-column">
+			<?php echo $lyricDetail['text'] ?>
+			</div>
+		</div>
+	</div>
