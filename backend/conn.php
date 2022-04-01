e34c1f6d4fa1225e0deb1b20f6f253e184bd67bf
Laurant Marijnissen <laurant@gigafyde.dev>
2022-04-01 12:30:48 +0200

    update mysql user

--- backend/conn.php
+++ backend/conn.php
@@ -1,8 +1,8 @@
 <?php
 $dbHost = 'localhost';
 $dbName = 'codecamp-c2';
-$dbUser = 'root';
-$dbPass = '';
+$dbUser = 'codecamp-c2';
+$dbPass = 'VEFIR2BACu21Qi1A24JEfu34keN73i';
 
 //Met behulp van PDO zetten we de connectie op, waarna we met setAttribute de manier van errormeldingen weergeven bepalen.
 $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
