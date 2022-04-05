ab3ff5d49df1f2846d7215277c93e6e465256536
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-04-05 16:04:41 +0200

    uitbreiding james bond

--- pages/acteurs/acteur/Roger-Moore/Roger-Moore.php
+++ pages/acteurs/acteur/Roger-Moore/Roger-Moore.php
@@ -71,11 +71,11 @@ include_once($path);
 
 
 	<div class="container black">
-		<img src="<?php echo baseUrl() ?>/public/img/spy.jpg" alt="map"/>
+		<img src="<?php echo baseUrl() ?>/public/img/moon.jpg" alt="map"/>
 		<div class="container-column black">
 			<h1>Moonraker: 1979</h1>
 			<p>
-			is een Britse spionagefilm uit 1977, de tiende in de James Bond-serie geproduceerd door Eon Productions. Het is de derde met Roger Moore als de fictieve geheim agent James Bond. De hoofdrollen worden vertolkt door Barbara Bach en Curt Jürgens. Het scenario was van Christopher Wood en Richard Maibaum, met een niet-gecrediteerde herschrijving door Tom Mankiewicz. 
+			is het derde James Bondboek geschreven in 1955 door Ian Fleming. In 1979 werd het boek verfilmd door EON Productions. De rol van James Bond werd vertolkt door Roger Moore en de film werd geregisseerd door Lewis Gilbert. Omdat sciencefictionfilms zeer succesvol bleken te zijn, ging Bond in deze film naar de ruimte terwijl in het originele boek Bond gewoon op de aarde blijft.
 			</p>
 			<a href="https://en.wikipedia.org/wiki/The_Spy_Who_Loved_Me_(film)">Wanna read more?</a>
 			
