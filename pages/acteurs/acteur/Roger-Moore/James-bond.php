4e601281fdab36127aed65425e7e2ebfb14b484b
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-04-06 11:24:17 +0200

    begin andere james bond pagina

--- pages/acteurs/acteur/Roger-Moore/James-bond.php
+++ pages/acteurs/acteur/Roger-Moore/James-bond.php
@@ -23,23 +23,22 @@ include_once($path);
 		<div class="container-column black">
 			<h1>James Bond (Roger Moore)</h1>
 			<p>
-				James Bond is a fictional character created by the British journalist and novelist Ian Fleming in 1952.
-				The literary character appeared in a series of twelve novels and two short story collections written by
-				Fleming and a number of continuation novels and spin-off works after Fleming's death in 1964. There have
-				been twenty-seven films in total, produced and released between 1962 and 2021.
+			James Bond is een personage bedacht door de Britse journalist en romanschrijver Ian Fleming in 1952. Het literaire personage verscheen in een reeks van twaalf romans en twee verhalenbundels geschreven door Fleming en een aantal vervolgromans en spin-off werken na Flemings dood in 1964. Er zijn in totaal zevenentwintig films geproduceerd en uitgebracht tussen 1962 en 2021.
 			</p>
-			<a href="https://en.wikipedia.org/wiki/Portrayal_of_James_Bond_in_film"> Wanna read more?</a>
+			<a href="<?php echo baseUrl() ?>/pages/acteurs/acteur/Roger-Moore/Roger-Moore.php">Meer te weten willen komen over Roger Moore?</a>
 		</div>
+
+	
 	</div>
 	<div class="container-column black">
 		<h1>James bond movies Roger Moore acted in</h1>
-		<a href="https://en.wikipedia.org/wiki/Live_and_Let_Die_(film)">Live and Let Die: 1973</a>
-		<a href="https://en.wikipedia.org/wiki/The_Man_with_the_Golden_Gun_(film)">The Man with the Golden Gun: 1974</a>
-		<a href="https://en.wikipedia.org/wiki/The_Spy_Who_Loved_Me_(film)">The Spy Who Loved Me: 1977</a>
-		<a href="https://en.wikipedia.org/wiki/Moonraker_(film)">Moonraker: 1979</a>
-		<a href="https://en.wikipedia.org/wiki/For_Your_Eyes_Only_(film)">For your eyes only: 1981</a>
-		<a href="https://en.wikipedia.org/wiki/Octopussy">Octopussy: 1983</a>
-		<a href="https://en.wikipedia.org/wiki/A_View_to_a_Kill">A view to a kill: 1985</a>
+		<a href="https://nl.wikipedia.org/wiki/Live_and_Let_Die_(film)">Live and Let Die: 1973</a>
+		<a href="https://nl.wikipedia.org/wiki/The_Man_with_the_Golden_Gun">The Man with the Golden Gun: 1974</a>
+		<a href="https://nl.wikipedia.org/wiki/The_Spy_Who_Loved_Me_(film)">The Spy Who Loved Me: 1977</a>
+		<a href="https://nl.wikipedia.org/wiki/Moonraker_(film)">Moonraker: 1979</a>
+		<a href="https://nl.wikipedia.org/wiki/For_Your_Eyes_Only_(film)">For your eyes only: 1981</a>
+		<a href="https://nl.wikipedia.org/wiki/Octopussy">Octopussy: 1983</a>
+		<a href="https://nl.wikipedia.org/wiki/A_View_to_a_Kill">A view to a kill: 1985</a>
 	</div>
 </div>
 
