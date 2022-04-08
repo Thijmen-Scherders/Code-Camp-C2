f81161a51d1780fdfee1aa318f052c882e31b788
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-04-08 11:54:49 +0200

    titel aanpas

--- pages/link/index.php
+++ pages/link/index.php
@@ -11,7 +11,7 @@ include_once($path);
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="<?php echo baseUrl() ?>/public/stylesheet.css">
-	<title>Links</title>
+	<title>Links naar film pagina's</title>
 </head>
 <body>
 
