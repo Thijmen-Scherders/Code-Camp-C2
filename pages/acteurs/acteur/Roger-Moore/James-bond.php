bf127858afe09a2d8eace29cbf5b555515c9f8bd
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-04-08 12:01:13 +0200

    teskt aanpas

--- pages/acteurs/acteur/Roger-Moore/James-bond.php
+++ pages/acteurs/acteur/Roger-Moore/James-bond.php
@@ -23,7 +23,9 @@ include_once($path);
 		<div class="container-column black">
 			<h1>James Bond (Roger Moore)</h1>
 			<p>
-			James Bond is een personage bedacht door de Britse journalist en romanschrijver Ian Fleming in 1952. Het literaire personage verscheen in een reeks van twaalf romans en twee verhalenbundels geschreven door Fleming en een aantal vervolgromans en spin-off werken na Flemings dood in 1964. Er zijn in totaal zevenentwintig films geproduceerd en uitgebracht tussen 1962 en 2021.
+			James Bond is een personage bedacht door de Britse journalist en romanschrijver Ian Fleming in 1952. 
+			Het literaire personage verscheen in een reeks van twaalf romans en twee verhalenbundels geschreven door Fleming en een aantal vervolgromans en spin-off werken na Flemings dood in 1964. 
+			Er zijn in totaal zevenentwintig films geproduceerd en uitgebracht tussen 1962 en 2021.
 			</p>
 			<a href="<?php echo baseUrl() ?>/pages/acteurs/acteur/Roger-Moore/Roger-Moore.php">Meer te weten willen komen over Roger Moore?</a>
 		</div>
