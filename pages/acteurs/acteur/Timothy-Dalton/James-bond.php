fde0e5d0668073d78bc016ff25063372750cd186
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2022-04-08 12:00:25 +0200

    Tekst iets anders gezet

--- pages/acteurs/acteur/Timothy-Dalton/James-bond.php
+++ pages/acteurs/acteur/Timothy-Dalton/James-bond.php
@@ -23,7 +23,9 @@ include_once($path);
 		<div class="container-column black">
 			<h1>James Bond (Timothy dalton)</h1>
 			<p>
-			James Bond is een personage bedacht door de Britse journalist en romanschrijver Ian Fleming in 1952. Het literaire personage verscheen in een reeks van twaalf romans en twee verhalenbundels geschreven door Fleming en een aantal vervolgromans en spin-off werken na Flemings dood in 1964. Er zijn in totaal zevenentwintig films geproduceerd en uitgebracht tussen 1962 en 2021
+			James Bond is een personage bedacht door de Britse journalist en romanschrijver Ian Fleming in 1952. 
+			
+			Het literaire personage verscheen in een reeks van twaalf romans en twee verhalenbundels geschreven door Fleming en een aantal vervolgromans en spin-off werken na Flemings dood in 1964. Er zijn in totaal zevenentwintig films geproduceerd en uitgebracht tussen 1962 en 2021
 			</p>
 			<a href="https://en.wikipedia.org/wiki/Portrayal_of_James_Bond_in_film"> Meer willen weten?</a>
 		</div>
